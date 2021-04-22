b66fc4c892005d3fdc95e32d4a7959bff7fb50a0
unknown <thibaultfert@gmail.com>
2021-04-22 18:07:24 +0200

    Suppression class ContactNotification (fichier + appel + include) non utilisee

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Service\Contact\ContactService;
-use App\Notification\ContactNotification;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,7 +24,7 @@ class MainController extends AbstractController
     /**
      * @Route("/main/contact", name="main_contact")
      */
-    public function formContact(Request $request, ContactNotification $contactNotification, ContactService $contactService)
+    public function formContact(Request $request, ContactService $contactService)
     {
         $contact = new Contact();
   
@@ -36,11 +35,6 @@ class MainController extends AbstractController
                                         // Analyse si soumis ou pas, si les champs sont remplis etc... 
                                         // Puis bind l'ensemble des champs dans l'objet associé (ici product)
         
-        /*if($form->isSubmitted() && $form->isValid()) {
-            $contactNotification->notify($contact); 
-            $this->addFlash('success','Votre message a bien été envoyé !');
-            return $this->redirectToRoute('main_home', []);
-        }*/
         if($form->isSubmitted() && $form->isValid()){
             $contactService->sendSimpleContactEmail($contact);
             $this->addFlash('success','Votre message a bien été envoyé, nous vous répondrons dès que possible !');
